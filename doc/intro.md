fff3579a2bbb6ead42cfedc488679bea0b6cad27
Jesse B. Miller <jesse@jessebmiller.com>
2016-05-28 15:45:42 +0000

    initial commit after Architecture

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cope
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cope "0.1.0-SNAPSHOT"
+  :description "Development Continuity (David Cope)"
+  :url "https://github.com/jessebmiller/cope"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
